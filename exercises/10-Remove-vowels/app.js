2f21c7da70011445377202d653282864f5efc2ff
Alejandro959521 <cabrerasm081@gmail.com>
2023-12-28 19:52:10 -0400

    terminados

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,6 +1,24 @@
 // Your code goes here
 
+let rapid = (frase) => {
 
+   let cadena=[]
+   
+   for (let letra of frase){
+    
+    if (letra == "a" || letra == "e" || letra == "i" || 
+    letra == "o" || letra == "u") 
+    {
+         continue
+
+    } else {cadena.push(letra.toUpperCase())}
+      
+   } 
+
+   return cadena.join('');
+
+
+}
 // Work above this line; do not change code below
 let str = "John";
 console.log(rapid(str));
